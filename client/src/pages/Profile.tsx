f1aa1032f93de214e0a88d91000d99363b86e515
mohamed amin <mohamed.amin@unilever.com>
2025-08-11 06:55:25 +0300

    getting logged in user data

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,42 +1,49 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/Profile.module.css";
 
-const API = axios.create({ baseURL: "http://localhost:5000", withCredentials: true });
+const API = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
 
-type UserProfile = {
+
+type User = {
   name: string;
   email: string;
-  avatarUrl?: string;
-};
-
-const dummyUser: UserProfile = {
-  name: "Mohamed Amin",
-  email: "mohamed.amin@example.com",
-  avatarUrl: "https://i.pravatar.cc/150?img=12", // صورة افتراضية من pravatar
 };
 
 export default function Profile() {
-  const user = dummyUser; // ممكن تجيب بيانات اليوزر من API أو Context
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const getUserData = async () => {
+      let res = await API.get("users/profile");
+      setUser(res.data)
+    };
+    getUserData();
+  }, []);
+console.log(user);
+
   const handleLogout = async () => {
-    await API.post("/auth/logout"); 
+    await API.post("/auth/logout");
     window.location.href = "/login";
   };
   return (
     <div className={styles.profileContainer}>
       <img
-        src={user.avatarUrl || "https://via.placeholder.com/120"}
+        src={"https://i.pravatar.cc/150?img=12"}
         alt="User Avatar"
         className={styles.avatar}
       />
-      <h1 className={styles.userName}>{user.name}</h1>
-      <p className={styles.userEmail}>{user.email}</p>
+      <h1 className={styles.userName}>{user?.name}</h1>
+      <p className={styles.userEmail}>{user?.email}</p>
 
       <div>
         <div className={styles.infoLabel}>Full Name</div>
-        <div className={styles.infoValue}>{user.name}</div>
+        <div className={styles.infoValue}>{user?.name}</div>
 
         <div className={styles.infoLabel}>Email Address</div>
-        <div className={styles.infoValue}>{user.email}</div>
+        <div className={styles.infoValue}>{user?.email}</div>
       </div>
 
       <button onClick={handleLogout}>Logout</button>
